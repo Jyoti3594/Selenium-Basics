724bd4f815ca838e8d9f45d857b70a4332563914
Jyoti Rani <jyoti35994@yahoo.com>
2021-07-16 13:02:55 -0700

    How to Perform Scrolling with in table and Window level using JavaScriptExecutor

--- src/ScrollingPage.java
+++ src/ScrollingPage.java
@@ -1,5 +1,6 @@
 
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
@@ -9,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.server.handler.SwitchToFrame;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 /*
 How to Perform Scrolling with in table and
@@ -29,13 +31,25 @@ public class ScrollingPage {
 
 
         //Use  Java Script  for scroll
-        JavascriptExecutor js = (JavascriptExecutor)driver;
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         js.executeScript("window.scrollBy(0,500)");
         Thread.sleep(3000L);
         js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
 
+        //sum up the total numbers given in table
+        List<WebElement> value = driver.findElements(By.cssSelector(".tableFixHead td:nth-child(4)"));
+        int sum = 0;
+        for (int i = 0; i < value.size(); i++) {
 
+            sum = sum + Integer.parseInt(value.get(i).getText());
 
+        }
+        System.out.println(sum);
+
+        //compare the Sum value is same as mentioned total amount on web page or not ?
+        driver.findElement(By.cssSelector(".totalAmount")).getText();
+        int total = Integer.parseInt(driver.findElement(By.cssSelector(".totalAmount")).getText().split(":")[1].trim());
+        Assert.assertEquals(sum, total);
     }
 }
